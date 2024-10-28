cf22d1500e43d4160daec61d8cba9392fb5086f1
jin.qin@hicity.world <1026958200@qq.com>
2024-10-28 14:01:13 +0800

    test1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  
+})
+export default defineConfig({
+  base:"/plob-api/",
+  build:{
+    outDir:"docs"
+  }
 })
